1f45f20281790b5d10e36dfe0f331e5c0093de0a
MartinQuivron <martin.quivron@student.junia.com>
2024-11-21 20:41:16 +0100

    chore(terraform): adding postgresql database, app service and service plan

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,3 +7,29 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_postgresql_flexible_server" "postgresql-server"{
+    name                = "postgresql-server-final-project15"
+    resource_group_name = azurerm_resource_group.rg.name
+    location            = azurerm_resource_group.rg.location
+    sku_name            = "B_Standard_B1ms"
+    storage_mb          = 32768
+    administrator_login = var.username_db
+    administrator_password = var.password_db
+    version = "16"
+    public_network_access_enabled = false
+}
+
+resource "azurerm_service_plan" "service_plan_project" {
+  name                = "service-plan-final-project15"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+resource "azurerm_app_service" "app_service_project" {
+  name                = "app-service-final-project15"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  app_service_plan_id = azurerm_service_plan.service_plan_project.id
+}
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,12 +1,24 @@
+variable "username_db" {
+  description = "Username for the database"
+  type        = string
+}
+
+variable "password_db" {
+  description = "Password for the database"
+  type        = string
+}
+
 variable "subscription_id" {
   description = "Subscription ID"
   type        = string
 }
+
 variable "location" {
   description = "Region for resources"
   type        = string
   default     = "France Central"
 }
+
 variable "resource_group_name" {
   description = "Name of resource group"
   type        = string
